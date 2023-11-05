bce761407e194a413719c6d29341b36f8c45b11d
Gomson-E <gomsonixx@gmail.com>
2023-11-05 17:43:31 +0900

    Initial Commit

---
+++ src/main/java/com/example/recode/RecodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecodeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecodeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recode/RecodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecodeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
