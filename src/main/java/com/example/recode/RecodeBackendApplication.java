23e173c2aaf223369603c1c153ad74476ff71da2
Gomson-E <gomsonixx@gmail.com>
2023-11-23 14:24:13 +0900

    FEAT : SpringBoot 로그인 화면 뜨지 않도록 설정

--- src/main/java/com/example/recode/RecodeBackendApplication.java
+++ src/main/java/com/example/recode/RecodeBackendApplication.java
@@ -2,8 +2,9 @@ package com.example.recode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RecodeBackendApplication {
 
     public static void main(String[] args) {
