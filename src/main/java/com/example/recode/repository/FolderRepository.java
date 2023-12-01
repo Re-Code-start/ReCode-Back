2a47c6ca2cf107585dc871164644da25626e0b06
Gomson-E <gomsonixx@gmail.com>
2023-12-01 17:57:42 +0900

    FEAT : 폴더 리스트 조회 기능

--- src/main/java/com/example/recode/repository/FolderRepository.java
+++ src/main/java/com/example/recode/repository/FolderRepository.java
@@ -1,11 +1,13 @@
 package com.example.recode.repository;
 
 import com.example.recode.domain.Folder;
+import com.example.recode.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
-
-
+    List<Folder> findAllByUser(User user);
 }
--- src/main/java/com/example/recode/service/FolderService.java
+++ src/main/java/com/example/recode/service/FolderService.java
@@ -3,12 +3,15 @@ package com.example.recode.service;
 import com.example.recode.domain.Folder;
 import com.example.recode.domain.User;
 import com.example.recode.dto.folder.FolderAddRequestDto;
+import com.example.recode.dto.folder.FolderListDto;
 import com.example.recode.repository.FolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,18 @@ public class FolderService {
     public Folder getFolder(Long folderId) {
         return folderRepository.findById(folderId)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 폴더입니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public List<FolderListDto> getFolderList(String userId) {
+        User user = userService.getUser(userId);
+        List<Folder> folders = folderRepository.findAllByUser(user);
+        return folders.stream()
+                .map(folder -> FolderListDto.builder()
+                        .id(folder.getId())
+                        .name(folder.getName())
+                        .build())
+                .collect(Collectors.toList());
     }
 
     @Transactional
