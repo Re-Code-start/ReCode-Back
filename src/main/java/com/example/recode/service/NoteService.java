30de804c19741ae08edb6fbaba853ba3be9868ae
Gomson-E <gomsonixx@gmail.com>
2023-12-01 18:00:43 +0900

    FEAT : 노트 리스트 조회 기능

--- src/main/java/com/example/recode/service/NoteService.java
+++ src/main/java/com/example/recode/service/NoteService.java
@@ -3,10 +3,14 @@ package com.example.recode.service;
 import com.example.recode.domain.Folder;
 import com.example.recode.domain.Note;
 import com.example.recode.dto.note.NoteAddRequestDto;
+import com.example.recode.dto.note.NoteListDto;
 import com.example.recode.repository.NoteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,18 @@ public class NoteService {
 
     private final NoteRepository noteRepository;
     private final FolderService folderService;
+
+    @Transactional(readOnly = true)
+    public List<NoteListDto> getNoteList(Long folderId) {
+        Folder folder = folderService.getFolder(folderId);
+        List<Note> notes = noteRepository.findAllByFolder(folder);
+        return notes.stream()
+                .map(note -> NoteListDto.builder()
+                        .id(note.getId())
+                        .title(note.getTitle())
+                        .build())
+                .collect(Collectors.toList());
+    }
 
     @Transactional
     public void addNote(NoteAddRequestDto dto) {
