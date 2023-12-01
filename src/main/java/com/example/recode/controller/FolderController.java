d30f9580182749bb1e6967a701f36f20a8273cbe
Gomson-E <gomsonixx@gmail.com>
2023-12-01 20:00:59 +0900

    FEAT : 폴더명 수정 Controller

--- src/main/java/com/example/recode/controller/FolderController.java
+++ src/main/java/com/example/recode/controller/FolderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,12 @@ public class FolderController {
     public ResponseEntity add(@RequestBody FolderAddRequestDto dto) {
         folderService.addFolder(dto);
         return ResponseEntity.ok("폴더 추가가 완료되었습니다.");
+    }
+
+    @PutMapping("/{folderId}/{folderName}")
+    public ResponseEntity<String> update(@PathVariable Long folderId, @PathVariable String folderName) {
+        folderService.updateFolderName(folderId, folderName);
+        return ResponseEntity.ok("폴더명 수정이 완료되었습니다.");
     }
 
     @DeleteMapping("/{folderId}")
