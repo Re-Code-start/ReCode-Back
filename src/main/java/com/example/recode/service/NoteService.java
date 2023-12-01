af87c6177ac454ff1dfda2f2b77f224886c7e631
Gomson-E <gomsonixx@gmail.com>
2023-12-01 19:44:54 +0900

    FEAT : 노트 삭제 기능

--- src/main/java/com/example/recode/service/NoteService.java
+++ src/main/java/com/example/recode/service/NoteService.java
@@ -54,4 +54,12 @@ public class NoteService {
         Note note = dto.toEntity(folder);
         noteRepository.save(note);
     }
+
+    @Transactional
+    public void deleteNote(Long noteId) {
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 노트입니다."));
+
+        noteRepository.delete(note);
+    }
 }
