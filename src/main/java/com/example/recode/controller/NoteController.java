036a65e1e5960cb441f693e37ad8833d1bdd54b7
Gomson-E <gomsonixx@gmail.com>
2023-12-01 18:00:33 +0900

    FEAT : 노트 리스트 조회 Controller

--- src/main/java/com/example/recode/controller/NoteController.java
+++ src/main/java/com/example/recode/controller/NoteController.java
@@ -1,13 +1,18 @@
 package com.example.recode.controller;
 
 import com.example.recode.dto.note.NoteAddRequestDto;
+import com.example.recode.dto.note.NoteListDto;
 import com.example.recode.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
 
     private final NoteService noteService;
+
+    @GetMapping("/{folderId}")
+    public List<NoteListDto> getList(@PathVariable Long folderId) {
+        return noteService.getNoteList(folderId);
+    }
 
     @PostMapping
     public ResponseEntity add(@RequestBody NoteAddRequestDto dto) {
