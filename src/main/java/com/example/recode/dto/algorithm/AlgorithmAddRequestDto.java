42482b70f1e7b5c59aaba0bfe62739ee28723517
Gomson-E <gomsonixx@gmail.com>
2023-12-13 21:05:48 +0900

    FEAT : 알고리즘 추가 요청 DTO

---
+++ src/main/java/com/example/recode/dto/algorithm/AlgorithmAddRequestDto.java
@@ -0,0 +1,23 @@
+package com.example.recode.dto.algorithm;
+
+import com.example.recode.domain.Algorithm;
+import com.example.recode.domain.Folder;
+import com.example.recode.domain.Note;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlgorithmAddRequestDto {
+
+    private Long folderId;
+
+    private Long noteId;
+
+    private String algorithmName;
+
+}
