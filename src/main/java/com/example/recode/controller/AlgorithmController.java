15557f36d56a6bca38412f56fa8685d630b9f9f2
Gomson-E <gomsonixx@gmail.com>
2023-12-13 21:06:01 +0900

    FEAT : 알고리즘 추가 Controller

--- src/main/java/com/example/recode/controller/AlgorithmController.java
+++ src/main/java/com/example/recode/controller/AlgorithmController.java
@@ -1,10 +1,13 @@
 package com.example.recode.controller;
 
+import com.example.recode.dto.algorithm.AlgorithmAddRequestDto;
 import com.example.recode.dto.algorithm.AlgorithmListDto;
 import com.example.recode.service.AlgorithmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,11 @@ public class AlgorithmController {
     @GetMapping("/list/{folderId}")
     public List<AlgorithmListDto> getList(@PathVariable Long folderId) {
         return algorithmService.getAlgorithmList(folderId);
+    }
+
+    @PostMapping
+    public List<AlgorithmListDto> add(@RequestBody AlgorithmAddRequestDto dto) {
+        return algorithmService.addAlgorithm(dto);
     }
 
 }
