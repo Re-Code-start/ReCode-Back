73ed7e8b0b575a2d5987c3dad080a362e4816146
Gomson-E <gomsonixx@gmail.com>
2023-12-13 21:28:52 +0900

    FEAT : 알고리즘 삭제 Controller

--- src/main/java/com/example/recode/controller/AlgorithmController.java
+++ src/main/java/com/example/recode/controller/AlgorithmController.java
@@ -4,6 +4,7 @@ import com.example.recode.dto.algorithm.AlgorithmAddRequestDto;
 import com.example.recode.dto.algorithm.AlgorithmListDto;
 import com.example.recode.service.AlgorithmService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,11 @@ public class AlgorithmController {
     @PutMapping("/{algorithmId}/{algorithmName}")
     public List<AlgorithmListDto> update(@PathVariable Long algorithmId, @PathVariable String algorithmName) {
         return algorithmService.updateAlgorithmName(algorithmId, algorithmName);
+    }
+
+    @DeleteMapping("/{algorithmId}")
+    public List<AlgorithmListDto> delete(@PathVariable Long algorithmId) {
+        return algorithmService.deleteAlgorithm(algorithmId);
     }
 
 }
