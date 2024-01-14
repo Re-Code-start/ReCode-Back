0d3bcccdc8e45a045f1f5974319e7d2771d4bbe8
Gomson-E <gomsonixx@gmail.com>
2024-01-14 17:06:24 +0900

    FEAT : 풀이 코드 수정 Controller

--- src/main/java/com/example/recode/controller/AnswerController.java
+++ src/main/java/com/example/recode/controller/AnswerController.java
@@ -1,10 +1,13 @@
 package com.example.recode.controller;
 
 import com.example.recode.dto.answer.AnswerAddRequestDto;
+import com.example.recode.dto.answer.AnswerUpdateRequestDto;
 import com.example.recode.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,12 @@ public class AnswerController {
     public ResponseEntity add(@RequestBody AnswerAddRequestDto dto) {
         answerService.addAnswer(dto);
         return ResponseEntity.ok("풀이 코드가 추가되었습니다.");
+    }
+
+    @PutMapping("/{answerId}")
+    public ResponseEntity update(@PathVariable Long answerId, @RequestBody AnswerUpdateRequestDto dto) {
+        answerService.updateAnswer(answerId, dto);
+        return ResponseEntity.ok("풀이 코드가 수정되었습니다.");
     }
 
 }
