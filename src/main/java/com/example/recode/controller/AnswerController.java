0d231ee77383e8cba098aa5b402ddc14ad87aff2
Gomson-E <gomsonixx@gmail.com>
2024-01-23 20:54:01 +0900

    FEAT : 풀이 코드 리스트 조회 Controller

--- src/main/java/com/example/recode/controller/AnswerController.java
+++ src/main/java/com/example/recode/controller/AnswerController.java
@@ -1,10 +1,12 @@
 package com.example.recode.controller;
 
 import com.example.recode.dto.answer.AnswerAddRequestDto;
+import com.example.recode.dto.answer.AnswerListDto;
 import com.example.recode.dto.answer.AnswerUpdateRequestDto;
 import com.example.recode.service.AnswerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,12 +14,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/answers")
 public class AnswerController {
 
     private final AnswerService answerService;
+
+    @GetMapping("/list/{groupId}")
+    public List<AnswerListDto> getList(@PathVariable Long groupId) {
+        return answerService.getAnswerList(groupId);
+    }
 
     @PostMapping
     public ResponseEntity add(@RequestBody AnswerAddRequestDto dto) {
