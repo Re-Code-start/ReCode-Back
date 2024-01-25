68f0802efe63175bf3734e4204cd665d6cc67a61
Gomson-E <gomsonixx@gmail.com>
2024-01-25 23:55:04 +0900

    FEAT : 피드백 등록 요청 DTO

---
+++ src/main/java/com/example/recode/dto/feedback/FeedbackAddDto.java
@@ -0,0 +1,39 @@
+package com.example.recode.dto.feedback;
+
+import com.example.recode.domain.Answer;
+import com.example.recode.domain.Feedback;
+import com.example.recode.domain.FeedbackType;
+import com.example.recode.domain.Users;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedbackAddDto {
+
+    @NotNull
+    private Long referenceId;          // 질문이나 풀이 코드 ID
+
+    private Integer lineNum;
+
+    @NotBlank
+    private String content;
+
+    public Feedback toEntityWithAnswer(Users user, Answer answer) {
+        return Feedback.builder()
+                .lineNum(lineNum)
+                .content(content)
+                .type(lineNum != null ? FeedbackType.LINE_BY_LINE_FEEDBACK : FeedbackType.COMMENT_FEEDBACK)
+                .user(user)
+                .answer(answer)
+                .build();
+    }
+
+}
