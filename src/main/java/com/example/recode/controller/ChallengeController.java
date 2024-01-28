d2cdb1bdf03e7da1d033b375c2a1148c1ec3dca9
Gomson-E <gomsonixx@gmail.com>
2024-01-28 13:48:15 +0900

    FEAT : 지난 챌린지 리스트 조회 Controller

--- src/main/java/com/example/recode/controller/ChallengeController.java
+++ src/main/java/com/example/recode/controller/ChallengeController.java
@@ -1,12 +1,17 @@
 package com.example.recode.controller;
 
+import com.example.recode.dto.challenge.ChallengeResponseDto;
 import com.example.recode.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
+
+    @GetMapping("/{groupId}/last")
+    public List<ChallengeResponseDto> getLastChallenges(@PathVariable Long groupId, @RequestParam int pageNumber) {
+        return challengeService.getLastChallenges(groupId, pageNumber);
+    }
 
     @PatchMapping("/close/{challengeId}")
     public ResponseEntity closeFeedbackVote(@PathVariable Long challengeId) {
