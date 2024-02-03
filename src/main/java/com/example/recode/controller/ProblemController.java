fb1cacfa9e4a64f8617d37ee296ca3738d6d0682
Gomson-E <gomsonixx@gmail.com>
2024-02-03 23:49:04 +0900

    FEAT : 문제 리스트 조회 Controller

---
+++ src/main/java/com/example/recode/controller/ProblemController.java
@@ -0,0 +1,25 @@
+package com.example.recode.controller;
+
+import com.example.recode.dto.problem.ProblemListDto;
+import com.example.recode.service.ProblemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/problems")
+public class ProblemController {
+
+    private final ProblemService problemService;
+
+    @GetMapping("/list/{challengeId}")
+    public List<ProblemListDto> getList(@PathVariable Long challengeId) {
+        return problemService.getProblemList(challengeId);
+    }
+
+}
