12a72f1bbd802eb90de7bf8bc785c6a94ecf03ef
Gomson-E <gomsonixx@gmail.com>
2024-02-04 01:50:51 +0900

    UPDATE : 풀이 코드 상세 조회 기능 - 피드백 포함

--- src/main/java/com/example/recode/service/AnswerService.java
+++ src/main/java/com/example/recode/service/AnswerService.java
@@ -2,6 +2,7 @@ package com.example.recode.service;
 
 import com.example.recode.domain.Algorithm;
 import com.example.recode.domain.Answer;
+import com.example.recode.domain.FeedbackType;
 import com.example.recode.domain.Group;
 import com.example.recode.domain.Problem;
 import com.example.recode.domain.Users;
@@ -9,6 +10,8 @@ import com.example.recode.dto.answer.AnswerAddRequestDto;
 import com.example.recode.dto.answer.AnswerListDto;
 import com.example.recode.dto.answer.AnswerResponseDto;
 import com.example.recode.dto.answer.AnswerUpdateRequestDto;
+import com.example.recode.dto.feedback.CommentFeedbackListDto;
+import com.example.recode.dto.feedback.LineFeedbackListDto;
 import com.example.recode.dto.note.NoteListDto;
 import com.example.recode.repository.AnswerRepository;
 import com.example.recode.repository.GroupRepository;
@@ -18,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.xml.stream.events.Comment;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,12 +40,22 @@ public class AnswerService {
         Answer answer = answerRepository.findById(answerId)
                 .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 풀이입니다."));
 
+        List<LineFeedbackListDto> lineFeedbacks = answer.getFeedbacks().stream()
+                .filter(feedback -> feedback.getType().equals(FeedbackType.LINE_FEEDBACK))
+                .map(LineFeedbackListDto::new)
+                .collect(Collectors.toList());
+
+        List<CommentFeedbackListDto> commentFeedbacks = answer.getFeedbacks().stream()
+                .filter(feedback -> feedback.getType().equals(FeedbackType.COMMENT_FEEDBACK))
+                .map(CommentFeedbackListDto::new)
+                .collect(Collectors.toList());
+
         return AnswerResponseDto.builder()
-                .title(answer.getProblem().getTitle())
-                .link(answer.getProblem().getLink())
                 .code(answer.getCode())
                 .comment(answer.getComment())
                 .algorithmList(answer.getAlgorithms().stream().map(Algorithm::getName).collect(Collectors.toList()))
+                .lineFeedbacks(lineFeedbacks)
+                .commentFeedbacks(commentFeedbacks)
                 .build();
     }
 
