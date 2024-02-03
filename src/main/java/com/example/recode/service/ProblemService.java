1e30fcf1a1e3ab20e442e07a0105333fc77c12d3
Gomson-E <gomsonixx@gmail.com>
2024-02-04 01:26:31 +0900

    FEAT : 문제 상세 조회 기능

--- src/main/java/com/example/recode/service/ProblemService.java
+++ src/main/java/com/example/recode/service/ProblemService.java
@@ -1,9 +1,14 @@
 package com.example.recode.service;
 
+import com.example.recode.domain.Algorithm;
 import com.example.recode.domain.Challenge;
+import com.example.recode.domain.Problem;
 import com.example.recode.domain.Users;
 import com.example.recode.dto.problem.ProblemListDto;
+import com.example.recode.dto.problem.ProblemResponseDto;
+import com.example.recode.dto.problem.SolvedMemberListDto;
 import com.example.recode.repository.ChallengeRepository;
+import com.example.recode.repository.ProblemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +20,32 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProblemService {
 
+    private final ProblemRepository problemRepository;
     private final ChallengeRepository challengeRepository;
     private final UserService userService;
+
+    public ProblemResponseDto getProblem(Long problemId) {
+        Problem problem = problemRepository.findById(problemId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 문제입니다."));
+
+        List<SolvedMemberListDto> solvedMembers = problem.getAnswers().stream()
+                .map(answer -> SolvedMemberListDto.builder()
+                        .answerId(answer.getId())
+                        .userId(answer.getUser().getId())
+                        .userImageUrl(answer.getUser().getImageUrl())
+                        .algorithmList(answer.getAlgorithms().stream().map(Algorithm::getName).collect(Collectors.toList()))
+                        .build())
+                .collect(Collectors.toList());
+
+        return ProblemResponseDto.builder()
+                .title(problem.getTitle())
+                .link(problem.getLink())
+                .category(problem.getCategory())
+                .difficulty(problem.getDifficulty())
+                .solvedMembers(solvedMembers)
+                .build();
+
+    }
 
     public List<ProblemListDto> getProblemList(Long challengeId) {
         Challenge challenge = challengeRepository.findById(challengeId)
