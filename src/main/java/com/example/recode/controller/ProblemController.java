4a13c3de6b155cec0cd6cabb3b824a4ada7c880d
Gomson-E <gomsonixx@gmail.com>
2024-02-08 17:18:29 +0900

    FEAT : 문제 추가 Controller

--- src/main/java/com/example/recode/controller/ProblemController.java
+++ src/main/java/com/example/recode/controller/ProblemController.java
@@ -1,11 +1,15 @@
 package com.example.recode.controller;
 
+import com.example.recode.dto.problem.ProblemAddRequestDto;
 import com.example.recode.dto.problem.ProblemListDto;
 import com.example.recode.dto.problem.ProblemResponseDto;
 import com.example.recode.service.ProblemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +30,12 @@ public class ProblemController {
     @GetMapping("/list/{challengeId}")
     public List<ProblemListDto> getList(@PathVariable Long challengeId) {
         return problemService.getProblemList(challengeId);
+    }
+
+    @PostMapping
+    public ResponseEntity add(@RequestBody ProblemAddRequestDto dto) {
+        problemService.addProblem(dto);
+        return ResponseEntity.ok("문제 추가가 완료되었습니다.");
     }
 
 }
